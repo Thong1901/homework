6c85bafe4201b13e60ac03074095a11a230417d5
Thong1901 <thongthanhmai129@gmail.com>
2025-08-07 15:37:47 +0700

    first

--- dist/expressplus.js
+++ dist/expressplus.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.ExpressPlus = void 0;
 var node_http_1 = require("node:http");
 var ExpressPlus = /** @class */ (function () {
     function ExpressPlus() {
@@ -43,6 +44,7 @@ var ExpressPlus = /** @class */ (function () {
     };
     return ExpressPlus;
 }());
+exports.ExpressPlus = ExpressPlus;
 var app = new ExpressPlus();
 app.get('/users', function (req, res) {
     res.writeHead(200, { 'Content-Type': 'application/json' });
