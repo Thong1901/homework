ba2cb64f345a5042738b4d303479f81fa63cffb3
Thong1901 <thongthanhmai129@gmail.com>
2025-08-08 21:19:35 +0700

    update

--- src/expressplus.ts
+++ src/expressplus.ts
@@ -1,4 +1,5 @@
-import { createServer } from 'node:http'
+import { createServer, IncomingMessage, ServerResponse } from 'node:http'
+import { parse } from 'node:url'
 
 export class ExpressPlus {
     private routes: { [method: string]: { [path: string]: Function } } = {}
@@ -23,15 +24,39 @@ export class ExpressPlus {
     public patch(path: string, handler: Function) {
         this.addRoute('PATCH', path, handler)
     }
+    private matchRoute(method: string, path: string): { handler?: Function, params?: Record<string, string> } {
+        const methodRoutes = this.routes[method]
+        if (!methodRoutes) return {}
+        for (const routePath in methodRoutes) {
+            const paramsNames: string[] = []
+            const regexPath = routePath.replace(/:([^\/]+)/g, (_, key) => {
+                paramsNames.push(key)
+                return '([^/]+)'
+            })
 
+            const regex = new RegExp(`^${regexPath}$`)
+            const match = path.match(regex)
+            if (match) {
+                const params: Record<string, string> = {}
+                paramsNames.forEach((name, index) => {
+                    params[name] = match[index + 1]
+                })
+                return { handler: methodRoutes[routePath], params }
+            }
+        }
+        return {}
+    }
     public listen(port: number, callback?: () => void) {
-        const server = createServer((req, res) => {
+        const server = createServer((req: IncomingMessage & { params?: any }, res: ServerResponse) => {
             const method = req.method || 'GET'
-            const path = req.url || ''
+            const url = parse(req.url || '', true)
+            const path = url.pathname || ''
 
-            const routeHandler = this.routes[method]?.[path]
-            if (routeHandler) {
-                routeHandler(req, res)
+            const { handler, params } = this.matchRoute(method, path)
+
+            if (handler) {
+                req.params = params
+                handler(req, res)
             } else {
                 res.writeHead(404, { 'Content-Type': 'text/plain' })
                 res.end('Not Found')
