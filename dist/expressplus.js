2f2361bb3a6ea209ae28f86c52d8ba6ec1e10d21
Thong1901 <thongthanhmai129@gmail.com>
2025-08-08 21:21:24 +0700

    update

--- dist/expressplus.js
+++ dist/expressplus.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ExpressPlus = void 0;
 var node_http_1 = require("node:http");
+var node_url_1 = require("node:url");
 var ExpressPlus = /** @class */ (function () {
     function ExpressPlus() {
         this.routes = {};
@@ -25,15 +26,43 @@ var ExpressPlus = /** @class */ (function () {
     ExpressPlus.prototype.patch = function (path, handler) {
         this.addRoute('PATCH', path, handler);
     };
+    ExpressPlus.prototype.matchRoute = function (method, path) {
+        var methodRoutes = this.routes[method];
+        if (!methodRoutes)
+            return {};
+        var _loop_1 = function (routePath) {
+            var paramsNames = [];
+            var regexPath = routePath.replace(/:([^\/]+)/g, function (_, key) {
+                paramsNames.push(key);
+                return '([^/]+)';
+            });
+            var regex = new RegExp("^".concat(regexPath, "$"));
+            var match = path.match(regex);
+            if (match) {
+                var params_1 = {};
+                paramsNames.forEach(function (name, index) {
+                    params_1[name] = match[index + 1];
+                });
+                return { value: { handler: methodRoutes[routePath], params: params_1 } };
+            }
+        };
+        for (var routePath in methodRoutes) {
+            var state_1 = _loop_1(routePath);
+            if (typeof state_1 === "object")
+                return state_1.value;
+        }
+        return {};
+    };
     ExpressPlus.prototype.listen = function (port, callback) {
         var _this = this;
         var server = (0, node_http_1.createServer)(function (req, res) {
-            var _a;
             var method = req.method || 'GET';
-            var path = req.url || '';
-            var routeHandler = (_a = _this.routes[method]) === null || _a === void 0 ? void 0 : _a[path];
-            if (routeHandler) {
-                routeHandler(req, res);
+            var url = (0, node_url_1.parse)(req.url || '', true);
+            var path = url.pathname || '';
+            var _a = _this.matchRoute(method, path), handler = _a.handler, params = _a.params;
+            if (handler) {
+                req.params = params;
+                handler(req, res);
             }
             else {
                 res.writeHead(404, { 'Content-Type': 'text/plain' });
